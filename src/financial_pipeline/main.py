84e3e625160c256a2519ffd3254b39954f8d9014
Fabio Esteves <fabiodesteves@gmail.com>
2026-04-15 14:25:33 +0100

    Update the main logic

--- src/financial_pipeline/main.py
+++ src/financial_pipeline/main.py
@@ -2,7 +2,7 @@ from financial_pipeline.extract import get_sp500_tickers, get_financial_data
 from financial_pipeline.load import load_raw_financials
 
 tickers = get_sp500_tickers()
-df_financials = get_financial_data(tickers, 10)
+df_financials = get_financial_data(tickers)
 load_raw_financials(df_financials)
 
 print("Pipeline complete.")
